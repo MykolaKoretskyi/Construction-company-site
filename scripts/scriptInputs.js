0fc15c6ee219ccc2b7a105d234f0208e39a5a494
MykolaKoretskyi <mikolakoreckij45@gmail.com>
2024-03-10 14:17:21 +0200

    added variable permission to send email

--- scripts/scriptInputs.js
+++ scripts/scriptInputs.js
@@ -3,8 +3,10 @@ const lastNameInput = document.getElementById("last-name-input");
 const emailInput = document.getElementById("email-input");
 const phoneInput = document.getElementById("phone-input");
 const addressInput = document.getElementById("address-input");
+const permissionSend = false;
 
-function sendEmail() {
+function sendEmail(event) {
+  event.preventDefault();
   let params = {
     nameValue: nameInput.value,
     lastNameValue: lastNameInput.value,
@@ -13,7 +15,22 @@ function sendEmail() {
     addressValue: addressInput.value,
   };
 
-  emailjs.send("service_g60qzha", "template_y0rvdn7", params).then(function () {
-    alert("Успішно відправлено! \n" + message);
-  });
+  if (permissionSend) {
+    emailjs
+      .send("service_g60qzha", "template_y0rvdn7", params)
+      .then(function () {
+        alert("Успішно відправлено! ");
+      });
+  } else {
+    alert(
+      "Шановний " +
+        nameInput.value +
+        " " +
+        lastNameInput.value +
+        " \nдякую вам за інтерес до даного сайту \n" +
+        "цей сайт розроблено як демо версію сайту, \n" +
+        "тому ваші дані не будуть ніяким способом \n" +
+        "оброблені або куди - небудь надіслані!"
+    );
+  }
 }
