b06867a9b495a94e740a7ddad7fae5ac478f3b0c
MykolaKoretskyi <mikolakoreckij45@gmail.com>
2024-03-10 12:02:19 +0200

    added send email logic

--- scripts/scriptInputs.js
+++ scripts/scriptInputs.js
@@ -1,23 +1,19 @@
-// Отримати посилання на інпути за їхніми ідентифікаторами
 const nameInput = document.getElementById("name-input");
 const lastNameInput = document.getElementById("last-name-input");
 const emailInput = document.getElementById("email-input");
 const phoneInput = document.getElementById("phone-input");
 const addressInput = document.getElementById("address-input");
 
-// Отримати посилання на кнопку
-const submitButton = document.querySelector(".client-button");
+function sendEmail() {
+  let params = {
+    nameValue: nameInput.value,
+    lastNameValue: lastNameInput.value,
+    emailValue: emailInput.value,
+    phoneValue: phoneInput.value,
+    addressValue: addressInput.value,
+  };
 
-// Додати обробник події для кнопки
-submitButton.addEventListener("click", function () {
-  // Отримати значення інпутів
-  const nameValue = nameInput.value;
-  const lastNameValue = lastNameInput.value;
-  const emailValue = emailInput.value;
-  const phoneValue = phoneInput.value;
-  const addressValue = addressInput.value;
-
-  // Створити повідомлення і вивести його
-  const message = `Ім’я: ${nameValue}\nПрізвище: ${lastNameValue}\nEmail: ${emailValue}\nТелефон: ${phoneValue}\nАдреса: ${addressValue}`;
-  alert(message);
-});
+  emailjs.send("service_g60qzha", "template_y0rvdn7", params).then(function () {
+    alert("Успішно відправлено! \n" + message);
+  });
+}
